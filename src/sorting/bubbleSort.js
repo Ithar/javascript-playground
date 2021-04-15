324cf9ff0c7211d34c57a0b5e95d2f18cbd4084f
ithar.malik <ithar.malik@bjss.com>
2021-04-15 13:50:54 +0100

    Added bubble sort

---
+++ src/sorting/bubbleSort.js
@@ -0,0 +1,28 @@
+// Optimised bubble sort; check no swaps and breaks to prevent unnecessary iterations
+
+function bubbleSort(arr){
+    let noSwaps;
+    for(let i = arr.length; i > 0; i--) {
+        noSwaps = true;
+
+        for(let j = 0; j < i - 1; j++){
+
+            console.log(arr, " " + arr[j] + "," + arr[j+1]);
+
+            if(arr[j] > arr[j+1]){
+               swap(arr, j);
+               noSwaps = false;
+            }
+        }
+        if(noSwaps) break;
+    }
+    return arr;
+}
+
+function swap(arr, j) {
+    let temp = arr[j];
+    arr[j] = arr[j+1];
+    arr[j+1] = temp;
+}
+
+bubbleSort([8,1,2,3,4,5,6,7]);
